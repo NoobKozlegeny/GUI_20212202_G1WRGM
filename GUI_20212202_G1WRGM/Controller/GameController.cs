9fcdb52bd46c522592354abf160f8ccfa7f654fb
NoobKozlegeny <toth00peter@gmail.com>
2022-03-31 22:41:58 +0200

    Add project files.

---
+++ GUI_20212202_G1WRGM/Controller/GameController.cs
@@ -0,0 +1,19 @@
+﻿using GUI_20212202_G1WRGM.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_20212202_G1WRGM.Controller
+{
+    public class GameController
+    {
+        IGameControl control;
+
+        public GameController(IGameControl control)
+        {
+            this.control = control;
+        }
+    }
+}
---
+++ GUI_20212202_G1WRGM/Renderer/Display.cs
@@ -0,0 +1,38 @@
+﻿using GUI_20212202_G1WRGM.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace GUI_20212202_G1WRGM.Renderer
+{
+    public class Display : FrameworkElement
+    {
+        IGameModel model;
+        Size size;
+
+        public void Resize(Size size)
+        {
+            this.size = size;
+        }
+
+        public void SetupModel(IGameModel model)
+        {
+            this.model = model;
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            if (model != null && size.Width > 50 && size.Height > 50)
+            {
+                drawingContext.DrawRectangle(Brushes.Black, new Pen(Brushes.Black, 0),
+                    new Rect(0, 0, 100, 100));
+            }
+        }
+    }
+}
