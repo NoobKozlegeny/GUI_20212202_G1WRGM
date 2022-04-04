b1ef2fd8baed3170996d8c71b50232d425d88ccf
szabados.krisztian@stud.uni-obuda.hu <szabados.krisztian@stud.uni-obuda.hu>
2022-04-05 01:29:49 +0200

    database created and connection set

---
+++ Data/DudeDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Data
+{
+    public class DudeDbContext :DbContext
+    {
+        public DudeDbContext()
+        {
+            Database.EnsureCreated();
+        }
+        public DudeDbContext(DbContextOptions<DudeDbContext> builder) : base(builder)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder
+                    .UseLazyLoadingProxies()
+                    .UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
+        }
+    }
+}
