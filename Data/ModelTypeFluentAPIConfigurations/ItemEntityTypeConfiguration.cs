f24faa16b60209820f98a178823e909ba4662d08
szabados.krisztian@stud.uni-obuda.hu <szabados.krisztian@stud.uni-obuda.hu>
2022-04-05 19:38:52 +0200

    Item model fluent api basic mapping configured

---
+++ Data/ModelTypeFluentAPIConfigurations/ItemEntityTypeConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.ModelTypeFluentAPIConfigurations
+{
+    class ItemEntityTypeConfiguration : IEntityTypeConfiguration<Item>
+    {
+        public void Configure(EntityTypeBuilder<Item> builder)
+        {
+            builder.ToTable("Item");
+            builder.HasKey(pk => pk.Id);
+            builder.Property(pk => pk.Id).ValueGeneratedOnAdd();
+            builder.Ignore(ItemNavPop => ItemNavPop.Map);
+
+            builder.HasOne(ItemNavProp => ItemNavProp.Map)
+                   .WithMany(MapNavprop => MapNavprop.Items)
+                   .HasForeignKey(ItemFk => ItemFk.MapLevel)
+                   .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
