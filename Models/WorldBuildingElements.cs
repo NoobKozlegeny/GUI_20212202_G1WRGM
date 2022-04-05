d2f887397e323ca63b601d79d64f89e1b0cbd283
szabados.krisztian@stud.uni-obuda.hu <szabados.krisztian@stud.uni-obuda.hu>
2022-04-05 14:38:01 +0200

    Models target platform fixed

--- Models/WorldBuildingElements.cs
+++ Models/WorldBuildingElements.cs
@@ -12,7 +12,6 @@ namespace Models
         public int Id { get; set; }
         public string Name { get; set; }
         public Point Position { get; set; }
-
         public int MapLevel { get; set; }
         public virtual Map Map { get; set; }
     }
