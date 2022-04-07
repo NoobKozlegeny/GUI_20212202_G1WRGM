e37ffccf17dd0e264e3ef7d0779973ad0dfc3b75
szabados.krisztian@stud.uni-obuda.hu <szabados.krisztian@stud.uni-obuda.hu>
2022-04-07 17:56:29 +0200

    map repo created for basic db CRUD

---
+++ Repository/Interfaces/IMapRepository.cs
@@ -0,0 +1,18 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    interface IMapRepository
+    {
+        public Map Read(int level);
+        public IQueryable<Map> ReadAll();
+        public void Create(int level);
+        public void Delete(int level);
+
+    }
+}
