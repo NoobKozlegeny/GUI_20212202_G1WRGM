89e78433f97d7e2581a96e8c94a5593452f964b5
szabados.krisztian@stud.uni-obuda.hu <szabados.krisztian@stud.uni-obuda.hu>
2022-04-07 19:54:50 +0200

    character repo crud implemented

---
+++ Repository/Interfaces/ICharacterRepository.cs
@@ -0,0 +1,17 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface ICharacterRepository
+    {
+        public Character Read(int id);
+        public IQueryable<Character> ReadAll();
+        public void Create(int id);
+        public void Delete(int id);
+    }
+}
