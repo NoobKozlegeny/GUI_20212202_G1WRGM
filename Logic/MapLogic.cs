82449a8aadfaa8e3529ebb12bdb18f0f006f819f
szabados.krisztian@stud.uni-obuda.hu <szabados.krisztian@stud.uni-obuda.hu>
2022-04-07 19:26:23 +0200

    map basic crud implemented

---
+++ Logic/MapLogic.cs
@@ -0,0 +1,38 @@
+﻿using Logic.Interfaces;
+using Models;
+using Repository.Interfaces;
+using System;
+using System.Linq;
+
+namespace Logic
+{
+    public class MapLogic : IMapLogic
+    {
+        IMapRepository mapRepo;
+
+        public MapLogic(IMapRepository mapRepo)
+        {
+            this.mapRepo = mapRepo;
+        }
+
+        public void Create(int level)
+        {
+            mapRepo.Create(level);
+        }
+
+        public void Delete(int level)
+        {
+            mapRepo.Delete(level);
+        }
+
+        public Map Read(int level)
+        {
+            return mapRepo.Read(level);
+        }
+
+        public IQueryable<Map> ReadAll()
+        {
+            return mapRepo.ReadAll();
+        }
+    }
+}
