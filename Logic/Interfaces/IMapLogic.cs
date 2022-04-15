7e04cb09ea54000c57c65ec16ebfabcc41e75906
Péter Tóth <toth00peter@gmail.com>
2022-04-15 13:15:48 +0200

    Literally can't resolve MSB3030 problem on master

---
+++ Logic/Interfaces/IMapLogic.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Interfaces
+{
+    public interface IMapLogic
+    {
+        public void Create(int level);
+        public Map Read(int level);
+        public IQueryable<Map> ReadAll();
+        public void Delete(int level);
+
+
+    }
+}
