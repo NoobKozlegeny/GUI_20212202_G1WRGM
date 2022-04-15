89ec8b951cc11cbffd8fbace2c239e22db0221d2
Péter Tóth <toth00peter@gmail.com>
2022-04-15 13:06:07 +0200

    Revert "deleted repos because they have already been created"
    This reverts commit a4f33eb4267b1dda66ec631e661773a0bf186be7.

---
+++ Repository/Interfaces/IWorldBuildingElementRepository.cs
@@ -0,0 +1,17 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    interface IWorldBuildingElementRepository
+    {
+        public WorldBuildingElement Read(int level, string name);
+        public IQueryable<WorldBuildingElement> ReadAll();
+        public void Create(WorldBuildingElement wbe);
+        public void Delete(int level, string name);
+    }
+}
