115c74e07644973dfa28f2ad8c7e0657dac12f03
Péter Tóth <toth00peter@gmail.com>
2022-04-15 13:07:33 +0200

    Revert "deleted repos because they have already been created"
    This reverts commit a4f33eb4267b1dda66ec631e661773a0bf186be7.

---
+++ Repository/Interfaces/ICharacterRepository.cs
@@ -0,0 +1,17 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    interface ICharacterRepository
+    {
+        public Character Read(int level, string name);
+        public IQueryable<Character> ReadAll();
+        public void Create(Character character);
+        public void Delete(int level, string name);
+    }
+}
---
+++ Repository/Interfaces/IItemRepository.cs
@@ -0,0 +1,17 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    interface IItemRepository
+    {
+        public Item Read(int level, string name);
+        public IQueryable<Item> ReadAll();
+        public void Create(Item item);
+        public void Delete(int level, string name);
+    }
+}
