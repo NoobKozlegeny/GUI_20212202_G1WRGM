18b9e8b0350998a141b4cbadec69acf983f948b8
Roleto <gulyaroland@gmail.com>
2022-04-16 20:03:37 +0200

    IGameControl and Model interfaces created

--- GUI_20212202_G1WRGM/Logic/IGameModel.cs
+++ GUI_20212202_G1WRGM/Logic/IGameModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace GUI_20212202_G1WRGM.Logic
 {
     public interface IGameModel
     {
+        public List<Item> StaticObject { get; set; }
+
+        public List<Item> MovingObject { get; set; }
+
+        public Character Player { get; set; }
+
+        public List<Character> Enemies { get; set; }
     }
 }
