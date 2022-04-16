6ab6f52902df5a5d3a7b75da50579fd3a13e8c75
Péter Tóth <toth00peter@gmail.com>
2022-04-16 19:59:04 +0200

    Changed Player's starting weapon

--- GUI_20212202_G1WRGM/Views/GameLevelControlView.xaml.cs
+++ GUI_20212202_G1WRGM/Views/GameLevelControlView.xaml.cs
@@ -82,7 +82,7 @@ namespace GUI_20212202_G1WRGM.Views
             List<Character> characters = new List<Character>()
             {
                 new Player() { Name = "Player", PathToImg = new Uri(System.IO.Path.Combine("Assets", "Characters", "Players", "Chad.png"), UriKind.RelativeOrAbsolute),
-                            Inventory = new Inventory() { PathToSelectedItemImg = new Uri(System.IO.Path.Combine("Assets", "Items", "Weapons", "Watergun.png"), UriKind.RelativeOrAbsolute) }},
+                            Inventory = new Inventory() { PathToSelectedItemImg = new Uri(System.IO.Path.Combine("Assets", "Items", "Weapons", "SuperShotgun.png"), UriKind.RelativeOrAbsolute) }},
                 new NPC() { Name = "NPC1", PathToImg = new Uri(System.IO.Path.Combine("Assets", "Characters", "NPCS", "TwistBrainlet.png"), UriKind.RelativeOrAbsolute), 
                             PathToWeaponImg = new Uri(System.IO.Path.Combine("Assets", "Items", "Weapons", "Watergun.png"), UriKind.RelativeOrAbsolute) },
                 new NPC() { Name = "NPC2", PathToImg = new Uri(System.IO.Path.Combine("Assets", "Characters", "NPCS", "TwistBrainlet.png"), UriKind.RelativeOrAbsolute),
